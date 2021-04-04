730d4d53cc41ba3b5d5c0448f7b7929ef9e1464e
kholilahqultsum <kholilahqultsum14@gmail.com>
2021-04-04 08:02:50 +0700

    final project

--- final-project/final_project.py
+++ final-project/final_project.py
@@ -1,11 +1,11 @@
 #Step 1: Import necessary package
-import smtplib #untuk log in ke email
+import smtplib #Python provides smtplib module, which defines an SMTP client session object that can be used to send mail to any Internet machine with an SMTP or ESMTP listener daemon
 #email.mime module is required to craft the email messages
 from email.mime.multipart import MIMEMultipart #untuk menambahkan lebih beberapa part pada email
 from email.mime.text import MIMEText #untuk menambahkan teks pada email
 from email.mime.base import MIMEBase
-from email import encoders
-import json
+from email import encoders #Encoder akan mengkodekan setiap jalur input yang aktif menjadi kode bilangan biner.
+import json #format data yang digunakan untuk pertukaran dan penyimpanan data
 
 #Step 2: Menambahkan variabel subjek, body email dan email penerima
 email_sender = input("Sender email: ")
@@ -16,10 +16,11 @@ for i in range(email):
     email_receiver = input("masukkan alamat email: ")
     daftar_email.append(email_receiver)
 
-rec =  ', '.join(daftar_email) 
+rec =  ', '.join(daftar_email) #mengkonversi atau mengubah variabel yang nilainya berformat array menjadi berformat string 
 
+#mengcopy unit dalam list ke file txt
 with open('receiver_list.txt', 'w') as f:
-    f.write(json.dumps(daftar_email))
+    f.write(json.dumps(daftar_email)) #encoding data
 
 msg = MIMEMultipart()
 msg['Subject'] = input("Subjek email: ")
@@ -31,7 +32,7 @@ msg.attach(MIMEText(body, 'plain'))
 
 #Step 3: Attachment Gambar
 filename = "Kholilah Qultsum_CV for KI.pdf"
-attachment = open("D:\\Kholilah Qultsum_CV for KI.pdf", "rb")
+attachment = open("D:\\Kholilah Qultsum_CV for KI.pdf", "rb") #rb untuk file yang bersifat binary
 
 #Step 4: Kirim email
 part = MIMEBase('application', 'octet-stream')
